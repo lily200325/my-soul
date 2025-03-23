33afb4f10d8c653955b46107374130e9cb29369a
lily200325 <1507285855@qq.com>
2025-03-23 11:06:18 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -6,16 +6,20 @@ const urlsToCache = [
   '/icon-512.png'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 }); 
